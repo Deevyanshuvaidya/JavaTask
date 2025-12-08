8bc578ef9d277d79697819e620e10441e7a3097e
Deevanshu Vaidya <146366010+Deevyanshuvaidya@users.noreply.github.com>
2025-12-08 14:33:31 +0530

    14th question

---
+++ 14.java
@@ -0,0 +1,41 @@
+abstract class Shape{
+    abstract double calculateArea();
+
+}
+
+class Circle extends Shape{
+    double radius;
+
+    Circle(double radius){
+        this.radius= radius;
+    }
+
+    @Override
+    double calculateArea(){
+        return Math.PI*radius*radius;
+    }
+}
+
+class Rectangle extends Shape{
+    double length, width;
+
+    Rectangle(double length, double width){
+        this.length=length;
+        this.width=width;
+    }
+
+    @Override
+    double calculateArea(){
+        return length*width;
+    }
+}
+
+public class Main{
+    public static void main(String args[]){
+        Shape circle= new Circle(5.0);
+        Shape rectangle = new Rectangle(4.0,6.0);
+
+        System.out.println("Circle Area : "+circle.calculateArea());
+        System.out.println("Rectangle Area : "+rectangle.calculateArea());
+    }
+}
